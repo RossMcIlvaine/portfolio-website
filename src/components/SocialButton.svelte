fc1d4fe51d1f5247e72b5b12554f011064134806
Ross McIlvaine <rossmcilvaine@gmail.com>
2024-03-24 14:39:23 -0500

    Create base components and social links

---
+++ src/components/SocialButton.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let url = "";
+    export let color = "";
+</script>
+
+<main>
+    <!-- svelte-ignore a11y-missing-content -->
+    <a target="_blank" href={url} 
+            class="fab fa-{color} flex text-{color} text-3xl md:text-socialIcon bg-white 
+                justify-center items-center w-20 md:w-16 h-20 md:h-16 
+                border-4 border-{color} border-solid rounded-full
+                transition-colors hover:bg-{color} hover:text-white duration-0.5" ></a>
+</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,26 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "github" : "#161B22",
+        "linkedin" : "#0A66C2",
+        "instagram" : "#E84E47",
+      },
+      fontSize:{
+        "socialIcon" : "1.875rem",
+      },
+      transitionDuration:{
+        "0.5" : "0.5s",
+      },
+    },
   },
+  safelist: [
+    {
+      pattern: /(text|bg|border)-(github|linkedin|instagram)/,
+      variants: ['hover'],
+    },
+  ],
   plugins: [],
 }
 
