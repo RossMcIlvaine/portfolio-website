e0cda0eca60d26dba2a403d717f320580584816c
Ross McIlvaine <rossmcilvaine@gmail.com>
2024-03-26 21:38:13 -0500

    To Top Button and new Icon

--- TODO.md
+++ TODO.md
@@ -2,18 +2,22 @@ Add Base Components
 ~~- Hero Section
 ~~- Experience
 ~~- Projects
-- Header and Nav
+~~- Education
+~~- Header and Nav
+- Mobile Nav
+~~- To Top Button
 
 Finalize Text
 - Hero Section
 - Experience
 - Projects
+- Education
 
 Finalize Styling
-- Interesting background?
 - Containers for each section
 - Get a font
 - Make sure it is reactive
+~~- Favicon
 
 Publish
 - Domain
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,29 +2,17 @@
     import "../app.css";
     import Header from "../components/Header.svelte";
     import Footer from "../components/Footer.svelte";
-
-    let nav = [
-        "About Me",
-        "Experience",
-        "Projects",
-        "Education"
-    ]
+	import ToTopButton from "../components/ToTopButton.svelte";
+	import Navigation from "../components/Navigation.svelte";
 </script>
 
 <main class="flex">
-    <div class="bg-white md:w-nav md:flex-shrink-0 md:shadow-binder md:z-10 hidden md:block text-center">
-        <ul class="list-none top-5 w-full sticky">
-            {#each nav as navText}
-                <li>
-                    <a href="#{navText}"class="text-black">{navText}</a>
-                </li>
-            {/each}
-        </ul>
-    </div>
+    <Navigation/>
     <div class="bg-mainBackground pt-10 pb-5 flex flex-col space-y-10 items-center">
         <Header/>
         <slot />
         <Footer/>
+        <ToTopButton/>
     </div>
 </main>
 
